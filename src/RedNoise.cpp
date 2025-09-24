204184c9d55ea711b8c977a8fb8dd7d5edba3f1f
Asher Bloom <si23870@bristol.ac.uk>
2025-09-24 12:37:07 +0100

    adding template to iterpolate function

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -3,35 +3,23 @@
 #include <Utils.h>
 #include <glm/glm.hpp>
 #include <vector>
-#include "glm/detail/func_vector_relational.hpp"
 
 #define WIDTH 320
 #define HEIGHT 240
 
-std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) {
-	if (numberOfValues <= 1) {
-		return std::vector<float>{from};
-	}
-	float interval = (to - from) / (numberOfValues - 1);
-	std::vector<float> values;
-	// (too?) simple fix that should allow us to compare floats
-	float tolerance = 0.01;
-	for (float n = from; !(n <= to + tolerance && n >= to - tolerance); n += interval)
-		values.emplace_back(n);
-	values.emplace_back(to);  // dodgy fix...... adds the final value (assumes it is equal to values.back()+interval)
-	return values;
-}
+template <typename T>
+std::vector<T> interpolate(T from, T to, int numberOfValues) {
+	if (numberOfValues <= 1)
+		return std::vector<T>{from};
 
-std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues) {
-	if (numberOfValues <= 1) {
-		return std::vector<glm::vec3>{from};
+	T interval = (to - from) / float{numberOfValues - 1.0f};
+	T currValue{from};
+	std::vector<T> values;
+
+	for (int i = 0; i < numberOfValues; i++) {
+		values.emplace_back(currValue);
+		currValue += interval;
 	}
-	glm::vec3 interval = (to - from) / glm::vec3(numberOfValues - 1);
-	std::vector<glm::vec3> values;
-	glm::vec3 tolerance(0.01);
-	for (glm::vec3 n = from; !(glm::all(glm::lessThanEqual(n, to + tolerance)) && glm::all(glm::greaterThanEqual(n, to - tolerance))); n += interval)
-		values.emplace_back(n);
-	values.emplace_back(to);
 	return values;
 }
 
@@ -40,7 +28,7 @@ void draw(DrawingWindow &window, std::vector<glm::vec3> &leftColumn, std::vector
 	for (size_t y = 0; y < window.height; y++) {
 		glm::vec3 leftPoint = leftColumn.at(y);
 		glm::vec3 rightPoint = rightColumn.at(y);
-		std::vector<glm::vec3> colours = interpolateThreeElementValues(leftPoint, rightPoint, window.width);
+		std::vector<glm::vec3> colours = interpolate(leftPoint, rightPoint, window.width);
 		for (size_t x = 0; x < window.width; x++) {
 			float red = colours.at(x).r;
 			float green = colours.at(x).g;
@@ -68,22 +56,14 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
-	glm::vec3 from(1, 4, 9.2);
-	glm::vec3 to(4, 1, 9.8);
-	auto result = interpolateThreeElementValues(from, to, 4);
-	std::cout << "size: " << result.size() << "\n";
-	for (auto vec : result) {
-		std::cout << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")\n";
-	}
+
 	glm::vec3 topLeft(255, 0, 0);		// red
 	glm::vec3 topRight(0, 0, 255);		// blue
 	glm::vec3 bottomRight(0, 255, 0);	// green
 	glm::vec3 bottomLeft(255, 255, 0);	// yellow
+	std::vector<glm::vec3> leftColumn = interpolate(topLeft, bottomLeft, window.height);
+	std::vector<glm::vec3> rightColumn = interpolate(topRight, bottomRight, window.height);
 
-	std::vector<glm::vec3> leftColumn = interpolateThreeElementValues(topLeft, bottomLeft, window.height);
-	std::vector<glm::vec3> rightColumn = interpolateThreeElementValues(topRight, bottomRight, window.height);
-
-	std::vector<float> vals = interpolateSingleFloats(255, 0, window.width);
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
